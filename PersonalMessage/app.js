25de99ca47c2daaaaaf6a2a3b68c87fbc38c1278
codingxllama <osamaalchalabi2@gmail.com>
2019-08-07 22:18:44 -0600

    Created a Counter html

--- PersonalMessage/app.js
+++ PersonalMessage/app.js
@@ -2,3 +2,44 @@ const sendBtn= document.querySelector('#sendBtn');
 const msgIn = document.querySelector("#msgIn");
 const msgOut = document.querySelector("#msgOut");
 
+sendBtn.addEventListener('click',sendMsg);
+
+// Creating Function body
+function sendMsg()
+{
+    // Getting msg content value
+    let msgContent= msgIn.value;
+
+     // Checking if the content is empty if yes then we display an alert to user
+    //else we allow the user to input data
+
+    if(msgContent==='')
+    {
+        alert("Please enter a valid Value in the message field");
+    }
+    else{
+     // placing the msg content value in the msgOut
+    
+    
+    msgOut.innerHTML=msgContent;
+    
+    // resetting the msgIn value to be null, because we want to reset it.
+    msgIn.value="";
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+ 
+
+   
+    
+}
--- QuoteGenerator/app.js
+++ QuoteGenerator/app.js
@@ -2,22 +2,23 @@ const possibleQuotes =[
     // Creating an array of possible quotes which contain objects of quotes.
     //Each object includes a name and quote
     {
-        author:'author 1',
-        quote:'quote 1'
+        author:'Dr. Seuss',
+        quote:'“Don\'t cry because it\'s over, smile because it happened.” '
     },
     {
-        author:'author 2',
-        quote:'quote 2'
+        author:'Marilyn Monroe',
+        quote:'“I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.” '
     },
     {
-        author:'author 3',
-        quote:'quote 3'
+        quote:'“A room without books is like a body without a soul.” ',
+        author:'Marcus Tullius Cicero'
     }
 ]
 
 
 const possibleBackground=[
-    
+    'url(Imgs/BackgroundImages/bg1.jpg)','url(Imgs/BackgroundImages/bg2.jpg)','url(Imgs/BackgroundImages/bg3.jpg)'
+       
 ]
 
 // Creating variables to target required elements
@@ -25,17 +26,25 @@ const possibleBackground=[
 const quoteBtn= document.querySelector('#quoteBtn');
 const quoteToDisplay = document.querySelector('#quote');
 const quoteAuthor= document.querySelector('#quoteAuthor');
-
+const bgChangerBtn=document.querySelector('#bgBtn');
+const appBody= document.querySelector('body');
 
 // Creating Event Listeners
 quoteBtn.addEventListener('click',function DisplayQuote()
 {
     var randomIndex=Math.floor(Math.random()*possibleQuotes.length);
     // getting a random author from the possibleQuotes Object Array
-    quoteAuthor.innerHTML= possibleQuotes[randomIndex].author;
+    quoteAuthor.innerHTML= '―'+possibleQuotes[randomIndex].author;
 
     // getting a random quote from the possibleQuotes Object Array
     quoteToDisplay.innerHTML=possibleQuotes[randomIndex].quote;
+});
 
+
+bgChangerBtn.addEventListener('click',function()
+{   
+    // appBody.style.backgroundImage=`url(possibleBackground[Math.floor(Math.random()*possibleBackground.length)])`;
+    var randomIndex=Math.floor(Math.random()*possibleBackground.length);
+    appBody.style.backgroundImage=possibleBackground[randomIndex];
 });
 
